e0cd6768d6bd41fa6dd27b4fde73e93850702cd1
grisha136 <71751494+grisha136@users.noreply.github.com>
2020-10-30 16:13:17 +0300

    30.10.2020

--- src/pages/AddBranch.jsx
+++ src/pages/AddBranch.jsx
@@ -4,6 +4,10 @@ import {Redirect} from "react-router-dom";
 
 // страница добавления ветки (каталога)
 
+const Option = (props)=> {
+    return <option value = {props.id}>{props.name_rus}</option>
+}
+
 export class AddBranch extends React.Component {
     constructor() {
         super();
@@ -13,6 +17,8 @@ export class AddBranch extends React.Component {
             referrer: null,
             name: "",
             name_rus: ""
+            options: [],
+            branch: 1
         }
     }
 
@@ -20,22 +26,30 @@ export class AddBranch extends React.Component {
         let formData = new FormData();
         formData.append("name",this.state.name);
         formData.append("name_rus",this.state.name_rus);
-        fetch(host + "addBranch",{
+        formData.append('html',`<h1>${this.state.name_rus}</h1>`);
+        formData.append('css', "");
+        formData.append('js',"");
+        formData.append('branch',this.state.branch);
+        fetch(host + "addPage",{
             method: "POST",
             body: formData
         })
-            .then(response=>response.json()).then(res=>{
+            .then(response=>response.json())
+            .then(res=>{
+                console.log("Страница раздела успешно создана");
                 this.setState({
                     referrer: "/branches/" }
-                    )
+                    }
+                 )
             })
+        })
     }
 
     handleInputChange(e){
         const target = e.target;
         const name = target.name;
         const value = target.value;
-
+        console.log(name, value)
         this.setState({
             [name]: value
         })
@@ -43,7 +57,7 @@ export class AddBranch extends React.Component {
 
     render() {
         const referrer = this.state.referrer;
-        if (referrer) return <Redirect to = {referrer}/>
+        if (referrer) return <Redirect to = {cmsPath + referrer}/>
         return <div>
             <h2 className="my-4 text-center">Добавить раздел сайта</h2>
             <div className = "mb-3">
@@ -51,6 +65,11 @@ export class AddBranch extends React.Component {
             </div>
             <div className = "mb-3">
                 <input name = "name_rus" value = {this.state.name_rus} onChange={this.handleInputChange} type = "text" className = "form-control" placeholder= "Name_RUS"/>
+            </div>
+            <div className = "mb-3">
+                <select name = "branch" onChange={this.handleInputChange} className= "form-control">
+                    {this.state.options}
+                </select>
             </div>
             <div className = "mb-3">
                 <button className= "btn btn-primary" onClick={this.handleSave}>Добавить</button>
